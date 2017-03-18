c98807431fad79190bb6b6cafa7aeadefb2a52b6
Ashok Lathwal <ashok.lathwal15@gmail.com>
2017-03-18 10:54:35 +0530

    Add files via upload

---
+++ AllPermutationWithOutDuplicate.java
@@ -0,0 +1,34 @@
+import java.io.*;
+class AllPermutationWithOutDuplicate
+{
+	static String swap(String str, int a, int b)
+	{
+		char[] arr=str.toCharArray();
+		char temp = arr[a];
+		arr[a] = arr[b];
+		arr[b]  = temp ;
+		return String.valueOf(arr);
+
+	}
+	static void allPermute(String str, int m, int n)
+	{
+		if(m ==n)
+			System.out.println(str);
+		else
+		{
+			for(int i=m;i<=n;i++)
+			{
+				str = swap(str,m,i);
+				allPermute(str,m+1,n);
+			    str = swap(str,m,i); // for backtracking
+			}
+		}
+	}
+
+	public static void main(String args[])
+	{
+		String str="ABCD";
+		int n=str.length();
+		allPermute(str,0,n-1);
+	}
+}
